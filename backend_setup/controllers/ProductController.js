fed8619b737d29cfe58bee13af30b9d615c7e4cb
shoronika <tanzila.ahmmed@g.bracu.ac.bd>
2024-03-13 20:48:31 +0600

    tanzila

--- backend_setup/controllers/ProductController.js
+++ backend_setup/controllers/ProductController.js
@@ -1,16 +1,68 @@
-const Product = require('../models/Product');
+const Category = require("../models/Category");
 
-exports.updateProductQuantity = async (req, res) => {
-  try {
-    const { productId, changeInQuantity } = req.body;
-    const product = await Product.findById(productId);
-    if (!product) {
-      return res.status(404).send('Product not found');
-    }
-    product.quantity += changeInQuantity;
-    await product.save();
-    res.json(product);
-  } catch (error) {
-    res.status(500).send(error.toString());
-  }
+const getCategories = async (_req, res) => {
+  const categories = await Category.find({}).populate("items", {
+    name: 1,
+    description: 1,
+    price: 1,
+  });
+
+  res.json(categories);
+};
+
+const getCategory = async (req, res) => {
+  const id = req.params.id;
+  const category = await Category.findById(id).populate("items", {
+    name: 1,
+    description: 1,
+    price: 1,
+  });
+
+  res.json(category);
+};
+
+const createCategory = async (req, res) => {
+  if (req.body.name === "" || !req.body.name)
+    return res.status(400).json({ message: "Empty category can not be added" });
+
+  const category = new Category({
+    name: req.body.name,
+  });
+
+  const savedCategory = await category.save();
+
+  res.status(201).json(savedCategory);
+};
+
+const editCategory = async (req, res) => {
+  const id = req.params.id;
+  const category = {
+    name: req.body.name,
+  };
+
+  const updatedCategory = await Category.findByIdAndUpdate(id, category, {
+    new: true,
+  }).populate("items", {
+    name: 1,
+    description: 1,
+    price: 1,
+  });
+
+  res.json(updatedCategory);
+};
+
+const deleteCategory = async (req, res) => {
+  const id = req.params.id;
+
+  await Category.findByIdAndRemove(id);
+
+  res.status(204).end();
+};
+
+module.exports = {
+  getCategories,
+  getCategory,
+  createCategory,
+  editCategory,
+  deleteCategory,
 };
